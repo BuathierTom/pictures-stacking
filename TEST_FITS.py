e6cb09318c2ea1d230c64de922597734193f3088
BuathierTom <97435667+BuathierTom@users.noreply.github.com>
2022-10-19 11:31:56 +0200

    update

--- TEST_FITS.py
+++ TEST_FITS.py
@@ -4,19 +4,19 @@ from astropy.utils.data import get_pkg_data_filename
 from astropy.io import fits
 
 from astropy.io import fits
-image_file = get_pkg_data_filename('./fits_tests/simple/fill0.fits')
+image_file = get_pkg_data_filename('./fits_tests/M13_blue/M13_blue_0001.fits')
 
-hdu_list = fits.open(image_file)
-hdu_list.info()
-image_data = hdu_list[0].data
-print(type(image_data))
-print(image_data.shape)
-hdu_list.close()
+# hdu_list = fits.open(image_file)
+# hdu_list.info()
+# image_data = hdu_list[0].data
+# print(type(image_data))
+# print(image_data.shape)
+# hdu_list.close()
+
+# ==> Avoir les info de l'images (taille//resolution .. etc)
 
 
 image_data = fits.getdata(image_file)
-print(type(image_data)) # Show the Python type for image_data
-print(image_data.shape) # Show the number of pixels per side in the 2-D image
-plt.imshow(image_data, cmap='gray')
+plt.imshow(image_data, cmap='winter_r')
 plt.colorbar()
 plt.show(block=True)
